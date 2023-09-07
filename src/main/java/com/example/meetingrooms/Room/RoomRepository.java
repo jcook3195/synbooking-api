4834cc400b700064720711265a29c6b2be2b3113
Natalie Barnes <natalie.barnes@projectsynergyga.com>
2023-09-07 08:23:47 -0400

    added cross origin allowance for localhost:3000

--- src/main/java/com/example/meetingrooms/Room/RoomRepository.java
+++ src/main/java/com/example/meetingrooms/Room/RoomRepository.java
@@ -1,12 +1,14 @@
 package com.example.meetingrooms.Room;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "rooms", path = "rooms")
+@CrossOrigin("http://localhost:3000")
 public interface RoomRepository extends MongoRepository<Room, String> {
 
     List<Room> findByName(@Param("name") String name);
