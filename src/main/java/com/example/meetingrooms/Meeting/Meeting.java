b7ead432da34c652042b115624377c3d6c898f91
saparrish <56564603+saparrish@users.noreply.github.com>
2023-09-08 09:28:09 -0400

    Adding description field

--- src/main/java/com/example/meetingrooms/Meeting/Meeting.java
+++ src/main/java/com/example/meetingrooms/Meeting/Meeting.java
@@ -3,49 +3,70 @@ package com.example.meetingrooms.Meeting;
 import org.springframework.data.annotation.Id;
 
 public class Meeting {
-    @Id private String id;
+    @Id
+    private String id;
 
     private String user;
     private String room;
     private String title;
-    private String startDateTime; //consider LocalDateTime
-    private String endDateTime; //for both of these
+    private String description;
+    private String startDateTime; // consider LocalDateTime
+    private String endDateTime; // for both of these
     private String attendees;
 
-    //getters and setters
+    // getters and setters
     public String getUser() {
         return user;
     }
+
     public void setUser(String user) {
         this.user = user;
     }
+
     public String getRoom() {
         return room;
     }
+
     public void setRoom(String room) {
         this.room = room;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getStartDateTime() {
         return startDateTime;
     }
+
     public void setStartDateTime(String startDateTime) {
         this.startDateTime = startDateTime;
     }
+
     public String getEndDateTime() {
         return endDateTime;
     }
+
     public void setEndDateTime(String endDateTime) {
         this.endDateTime = endDateTime;
     }
+
     public String getAttendees() {
         return attendees;
     }
+
     public void setAttendees(String attendees) {
         this.attendees = attendees;
     }
