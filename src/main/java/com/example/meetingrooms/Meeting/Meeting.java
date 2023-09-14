29cee89bf9a64a9e2b49c804938c8d60a8d20388
saparrish <56564603+saparrish@users.noreply.github.com>
2023-09-14 08:56:43 -0400

    Updated Meeting, MeetingController for crud accessibility

--- src/main/java/com/example/meetingrooms/Meeting/Meeting.java
+++ src/main/java/com/example/meetingrooms/Meeting/Meeting.java
@@ -15,6 +15,14 @@ public class Meeting {
     private String attendees;
 
     // getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getUser() {
         return user;
     }
