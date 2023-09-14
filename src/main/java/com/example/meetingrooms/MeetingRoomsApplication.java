72561f4e1d2386e87f4a2eb32be965221b628147
saparrish <56564603+saparrish@users.noreply.github.com>
2023-09-14 07:39:55 -0400

    Removed test comment line from initial git tests

--- src/main/java/com/example/meetingrooms/MeetingRoomsApplication.java
+++ src/main/java/com/example/meetingrooms/MeetingRoomsApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// test comment 2 :)
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class MeetingRoomsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetingRoomsApplication.class, args);
 	}
-
 }
