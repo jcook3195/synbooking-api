96703d0e901d8c6b18bc3e933a256c824e256818
Natalie Barnes <natalie.barnes@projectsynergyga.com>
2023-09-15 07:52:30 -0400

    moved cross origin statements to controllers for new crud methods

--- src/main/java/com/example/meetingrooms/Meeting/MeetingController.java
+++ src/main/java/com/example/meetingrooms/Meeting/MeetingController.java
@@ -1,11 +1,12 @@
 package com.example.meetingrooms.Meeting;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class MeetingController {
     @Autowired
     private MeetingRepository repo;
--- src/main/java/com/example/meetingrooms/Users/UserController.java
+++ src/main/java/com/example/meetingrooms/Users/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepo repo;
