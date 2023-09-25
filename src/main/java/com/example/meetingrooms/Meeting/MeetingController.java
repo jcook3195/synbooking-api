fd634457fd365b332ad35a4b103e116edde6f73a
saparrish <56564603+saparrish@users.noreply.github.com>
2023-09-25 13:56:13 -0400

    Current progress on spring security. DOES NOT ALLOW MEETING CREATION!

--- src/main/java/com/example/meetingrooms/Meeting/MeetingController.java
+++ src/main/java/com/example/meetingrooms/Meeting/MeetingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping
 @CrossOrigin("http://localhost:3000")
 public class MeetingController {
     @Autowired
--- src/main/java/com/example/meetingrooms/SecurityConfig.java
+++ src/main/java/com/example/meetingrooms/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.example.meetingrooms;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+    // builds default login screen
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin().loginPage("/login").permitAll();
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        // http.httpBasic(withDefaults());
         return http.build();
     }
 
@@ -19,5 +31,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.logout(Customizer.withDefaults());
         return http.build();
+    }
+
+    // allows requests front frontend
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
