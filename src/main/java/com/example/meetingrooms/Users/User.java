43101deb752c2659b0259d656aaa4cf9f3acb14c
saparrish <56564603+saparrish@users.noreply.github.com>
2023-10-06 16:29:42 -0400

    updates from before security+

--- src/main/java/com/example/meetingrooms/Users/User.java
+++ src/main/java/com/example/meetingrooms/Users/User.java
@@ -1,5 +1,8 @@
 package com.example.meetingrooms.Users;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
